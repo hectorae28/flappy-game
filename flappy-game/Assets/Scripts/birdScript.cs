b6a7401cd83fce772cc25313e7c92059e6b7a15f
Hector Archer <hectorae28@gmail.com>
2022-07-27 18:41:02 -0400

    android controls

--- flappy-game/Assets/Scripts/birdScript.cs
+++ flappy-game/Assets/Scripts/birdScript.cs
@@ -16,7 +16,7 @@ public class birdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Space)|Input.touchCount>0){
             Jump();
         }
     }
